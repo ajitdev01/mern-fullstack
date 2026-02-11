7084a43c13b8d385682050f02c502b79f1a0a3cc
Ajit Dev <ajitk23192@gmail.com>
2026-02-11 15:29:04 +0530

    Add files via upload
    Setup using NPM

---
+++ Lecture-4-setup/Components/Gretting.jsx
@@ -0,0 +1,10 @@
+
+function Gretting(name) {
+  return (
+    <>
+    <h1>Hello Sir/Mam {name.name} </h1>
+    </>
+  )
+}
+
+export default Gretting
---
+++ Lecture-4-setup/src/App.jsx
@@ -0,0 +1,16 @@
+import Gretting from '../Components/Gretting'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Gretting name="Nilam"/>
+    <Gretting name="Parvati"/>
+    <Gretting name="Sir"/>
+     
+    </>
+  )
+}
+
+export default App
